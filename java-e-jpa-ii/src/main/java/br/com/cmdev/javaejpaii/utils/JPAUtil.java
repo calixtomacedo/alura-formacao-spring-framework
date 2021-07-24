48558cfba3c33adfd2bd2da5558861a601fb506e
Calixto Macedo <calixto.macedo@gmail.com>
2021-07-24 12:36:21 -0300

    Identação

--- java-e-jpa-ii/src/main/java/br/com/cmdev/javaejpaii/utils/JPAUtil.java
+++ java-e-jpa-ii/src/main/java/br/com/cmdev/javaejpaii/utils/JPAUtil.java
@@ -5,9 +5,9 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
 public class JPAUtil {
-	
-private static final EntityManagerFactory FACTORY = Persistence.createEntityManagerFactory("java-e-jpa-ii");
-	
+
+	private static final EntityManagerFactory FACTORY = Persistence.createEntityManagerFactory("java-e-jpa-ii");
+
 	public static EntityManager getEntityManager() {
 		return FACTORY.createEntityManager();
 	}
