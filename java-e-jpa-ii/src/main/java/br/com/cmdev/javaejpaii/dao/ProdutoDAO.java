62e1f09fd56e142f3e857df656a3859fc198b615
Calixto Macedo <calixto.macedo@gmail.com>
2021-07-24 06:55:46 -0300

    04.Criteria API

--- java-e-jpa-ii/src/main/java/br/com/cmdev/javaejpaii/dao/ProdutoDAO.java
+++ java-e-jpa-ii/src/main/java/br/com/cmdev/javaejpaii/dao/ProdutoDAO.java
@@ -5,6 +5,7 @@ import java.time.LocalDate;
 import java.util.List;
 
 import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Predicate;
@@ -19,7 +20,7 @@ public class ProdutoDAO {
 	public ProdutoDAO(EntityManager entityManager) {
 		this.entityManager = entityManager;
 	}
-	
+
 	public void cadastrar(Produto produto) {
 		this.entityManager.persist(produto);
 	}
@@ -28,7 +29,7 @@ public class ProdutoDAO {
 		String jpql = "SELECT p FROM Produto p";
 		return this.entityManager.createQuery(jpql, Produto.class).getResultList();
 	}
-	
+
 	public void atualizar(Produto produto) {
 		this.entityManager.merge(produto);
 	}
@@ -45,29 +46,54 @@ public class ProdutoDAO {
 		String jpql = "SELECT p FROM Produto p WHERE p.nome LIKE :pNome";
 		return this.entityManager.createQuery(jpql, Produto.class).setParameter("pNome", nome).getResultList();
 	}
-	
+
 	public List<Produto> buscarPorLike(String nome) {
 		String jpql = "SELECT p FROM Produto p WHERE UPPER(p.nome) LIKE UPPER(:pNome)";
-		return this.entityManager.createQuery(jpql, Produto.class).setParameter("pNome", "%"+nome+"%").getResultList();
+		return this.entityManager.createQuery(jpql, Produto.class).setParameter("pNome", "%" + nome + "%").getResultList();
 	}
-	
+
 	public List<Produto> buscarPorCategoria(String nome) {
 		String jpql = "SELECT p FROM Produto p WHERE p.categoria.descricao = :pDescricao";
 		return this.entityManager.createQuery(jpql, Produto.class).setParameter("pDescricao", nome).getResultList();
 	}
-	
+
 	public List<Produto> buscarPorCategoriaNamedQuery(String nome) {
 		return this.entityManager.createNamedQuery("Produto.buscaPorCategoria", Produto.class).setParameter("pDescricao", nome).getResultList();
 	}
-	
-	
+
 	public BigDecimal buscarPrecoDoProdutoComNome(String nome) {
 		String jpql = "SELECT p.preco FROM Produto p WHERE p.nome = :pNome";
 		return this.entityManager.createQuery(jpql, BigDecimal.class).setParameter("pNome", nome).getSingleResult();
 	}
-	
-	public List<Produto> buscarProdutoComCriteria(String nome, BigDecimal preco, LocalDate dataCadastro) {
+
+	public List<Produto> buscarPorParametros(String nome, BigDecimal preco, LocalDate dataCadastro) {
+		String jpql = "SELECT p FROM Produto p WHERE 1=1 ";
+		if (nome != null && !nome.trim().isEmpty()) {
+			jpql = " AND p.nome = :nome ";
+		}
+		if (preco != null) {
+			jpql = " AND p.preco = :preco ";
+		}
+		if (dataCadastro != null) {
+			jpql = " AND p.dataCadastro = :dataCadastro ";
+		}
 		
+		TypedQuery<Produto> query = entityManager.createQuery(jpql, Produto.class);
+		if (nome != null && !nome.trim().isEmpty()) {
+			query.setParameter("nome", nome);
+		}
+		if (preco != null) {
+			query.setParameter("preco", preco);
+		}
+		if (dataCadastro != null) {
+			query.setParameter("dataCadastro", dataCadastro);
+		}
+
+		return query.getResultList();
+	}
+
+	public List<Produto> buscarProdutoComCriteria(String nome, BigDecimal preco, LocalDate dataCadastro) {
+
 		CriteriaBuilder builder = entityManager.getCriteriaBuilder();
 		CriteriaQuery<Produto> query = builder.createQuery(Produto.class);
 		Root<Produto> from = query.from(Produto.class);
@@ -82,7 +108,7 @@ public class ProdutoDAO {
 			filtros = builder.and(filtros, builder.equal(from.get("dataCadastro"), dataCadastro));
 		}
 		query.where(filtros);
-		
+
 		return entityManager.createQuery(query).getResultList();
 	}
 }
