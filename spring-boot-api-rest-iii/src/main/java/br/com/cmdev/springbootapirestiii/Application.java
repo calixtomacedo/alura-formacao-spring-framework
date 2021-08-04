1afb7b4c1cb9207de3cff93d82e7afae2d8f9888
Calixto Macedo <calixto.macedo@gmail.com>
2021-08-04 13:07:23 -0300

    05.Deploy com Docker e na nuvem - Fim do curso

--- spring-boot-api-rest-iii/src/main/java/br/com/cmdev/springbootapirestiii/Application.java
+++ spring-boot-api-rest-iii/src/main/java/br/com/cmdev/springbootapirestiii/Application.java
@@ -2,8 +2,6 @@ package br.com.cmdev.springbootapirestiii;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
@@ -13,15 +11,14 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSpringDataWebSupport
 @EnableCaching
 @EnableSwagger2
-public class Application extends SpringBootServletInitializer {
+//public class Application extends SpringBootServletInitializer {
+public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
-	
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-		return builder.sources(Application.class);
-	}
 
+	/*
+	 * @Override protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) { return builder.sources(Application.class); }
+	 */
 }
