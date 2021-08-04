b28881abfaea53deb3e985e38cb60e464f017a58
Calixto Macedo <calixto.macedo@gmail.com>
2021-08-04 08:33:05 -0300

    04.Deploy

--- spring-boot-api-rest-iii/src/main/java/br/com/cmdev/springbootapirestiii/Application.java
+++ spring-boot-api-rest-iii/src/main/java/br/com/cmdev/springbootapirestiii/Application.java
@@ -2,6 +2,8 @@ package br.com.cmdev.springbootapirestiii;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
@@ -11,10 +13,15 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSpringDataWebSupport
 @EnableCaching
 @EnableSwagger2
-public class Application {
+public class Application extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(Application.class);
 	}
 
 }
